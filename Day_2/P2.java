b5c9e39bbc5dc7bd4cdd3617fdc2e191c35581b2
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-08-27 19:45:14 +0530

    day2 assignments

---
+++ Day_2/P2.java
@@ -0,0 +1,35 @@
+class P2
+{
+	public static void main(String args[])
+	{
+	  int i=1;
+	  System.out.println("Odd numbers:");
+
+  	  while(i<=20)	  
+	  { 
+	    if(i%2==1)
+		{
+
+	      System.out.println(i);
+	      }  
+		i++;
+
+	  }
+ 	 System.out.println("");
+
+        int a=1;
+	 System.out.println("Even numbers:");
+
+        while(a<=20)	  
+       { 
+	  if(a%2==0)
+	  {
+	    System.out.println(a);
+	  }  
+	 a++;
+
+	  }
+
+
+	}
+}
