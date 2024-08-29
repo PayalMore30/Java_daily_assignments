01c1427b32b274dbcc00a3fbd1dcd48202137cbd
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-08-29 23:12:23 +0530

    day4 assignment

---
+++ Day_4/P1.java
@@ -0,0 +1,12 @@
+class P1
+{
+	public static void main(String args[])
+	{   
+  	  for(char i='A';i<='I';i++)	  
+	  { 
+		System.out.print(i++ + " ");
+	  }
+	  
+
+	}
+}
---
+++ Day_4/P4.java
@@ -0,0 +1,15 @@
+class P4
+{
+	public static void main(String args[])
+	{
+	  for(int i=0;i<4;i++) 
+        { 
+            for(int j=1;j<=4;j++) 
+            {
+                System.out.print("1 ");
+            }
+            System.out.println(); 
+        }
+
+       }
+}
