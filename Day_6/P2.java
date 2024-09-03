fadbe62ac3d4318c9f7248e1910bc02917aa6b9f
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-09-03 23:54:05 +0530

    day6 assignment

---
+++ Day_6/P2.java
@@ -0,0 +1,33 @@
+class P2
+{
+   public static void main(String args[])
+    {
+	int marks=80;
+	
+	if(marks<=90 && marks<=100)
+	{
+	    System.out.println("A grade");
+
+        }
+	else if(marks<=80 && marks<=89)
+	{
+	    System.out.println("B grade");
+
+        }
+	else if(marks<=70 && marks<=79)
+	{	     
+	    System.out.println("C grade");
+	}
+	else if(marks<=60 && marks<=69)
+	{	     
+	    System.out.println("D grade");
+	}
+	else
+	{	     
+	    System.out.println("F grade");
+	}
+
+
+}
+}
+ 	
---
+++ Day_6/P7.java
@@ -0,0 +1,23 @@
+class P7
+{
+	public static void main(String args[])
+	{
+	  
+	  for(int i=1;i<=3;i++) 
+        { 
+	    char ch='A';
+
+            for(int j=1;j<=4;j++) 
+            {  
+		{
+		      System.out.print(ch+ " ");
+		       ch++;                  
+		}
+	
+            }
+            System.out.println();
+ 
+        }
+
+       }
+}
