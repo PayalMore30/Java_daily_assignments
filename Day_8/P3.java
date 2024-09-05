da5d72d0e51ff69dfadd9dcea1fbb828605fad1b
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-09-05 20:53:13 +0530

    day8 assignment

---
+++ Day_8/P3.java
@@ -0,0 +1,18 @@
+class P3
+{
+	public static void main(String args[])
+	{
+	 
+	  for(int i=1;i<=4;i++) 
+        { 
+            for(int j=1;j<=i;j++) 
+	     {
+		 System.out.print("01 ");
+		
+	    }
+
+            System.out.println(); 
+        }
+
+       }
+}
