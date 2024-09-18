daad912525b7909b9473a1e1c3682f92c88de7a2
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-09-18 23:41:19 +0530

    day12 assignment

---
+++ Day_12/P3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class P3{
+	public static void main(String args[])
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter 1st number:"); 
+	    int a=sc.nextInt();
+	    System.out.println("Enter 2nd number:"); 
+	    int b=sc.nextInt();
+
+            System.out.println("The addition of " +a*a*a +" & " +b*b*b +" is: "+(a*a*a + b*b*b)); 
+	    System.out.println("The Substraction of " +a*a +" & " +b*b +" is: "+(a*a - b*b)); 
+
+
+}
+}
--- Day_12/P5.java
+++ Day_12/P5.java
@@ -1,15 +1,14 @@
-import java.util.*;
-class P4
+class P5
 {
 	public static void main(String args[])
 	{
-	  int a=7;
+	  
 	  for(int i=1;i<=4;i++) 
         { 
             for(int j=1;j<=i;j++) 
             {  
 		
-		      System.out.print("0  ");		
+		      System.out.print("*   ");		
 
             }
             System.out.println(); 
