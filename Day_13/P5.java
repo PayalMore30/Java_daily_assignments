f8370095a70a5acf5c54aa4fcf9afdb267d45b89
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-09-24 23:06:01 +0530

    day13 assignment

---
+++ Day_13/P5.java
@@ -0,0 +1,16 @@
+class P5
+{
+   public static void main(String [] args)
+   {
+      int arr[]={10,20,30,40,50};
+     System.out.print("Array element is: ");
+
+      for(int i=0; i<arr.length; i++)
+      {	
+    	 System.out.print(arr[i]+" ");
+
+      }
+     
+     }
+	
+  }
