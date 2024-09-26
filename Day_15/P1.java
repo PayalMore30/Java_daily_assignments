2c8e49cef2088e69122011c17cfe383aebbc518c
payalmore30102004@gmail.com <anishmore2002@gmail.com>
2024-09-26 22:45:46 +0530

    day15 assignment

---
+++ Day_15/P1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class P1
+{
+   public static void main(String [] args)
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter min number of series:");
+      int a=sc.nextInt();
+      System.out.print("Enter max number of series: ");
+      int b=sc.nextInt();
+
+       System.out.print("series of even numbers Ranging between " +a +" and " +b +" is:");
+      for(int i=a; i<=b; i++)
+      {	 
+         if(a%2==0)
+	{
+    	 
+         System.out.print(a+ " ");
+        }
+        a++;
+     }
+	
+  }
+}
---
+++ Day_15/P5.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class P5
+{
+   public static void main(String [] args)
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter 1st numbers: ");
+      int a=sc.nextInt();
+      System.out.print("Enter 2nd numbers: ");
+      int b=sc.nextInt();
+      System.out.print("Enter 3rd numbers: ");
+      int c=sc.nextInt();
+
+      System.out.print("The maximum number amongst "+a + "," +b +" & "+c + " is: ");
+
+         if(a>=b && a>+c)
+	{  	 
+           System.out.print(" "+a);
+        }
+	else if(b>=c && b>=a)
+	{  	 
+           System.out.print(" "+b);
+        }
+        else{
+      	  System.out.print(" "+c);
+       }   
+	
+     }
+}
